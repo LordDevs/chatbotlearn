6df4531b8f5e3019fb0f85bb38ce17bd7ad65ca6
michel.lopes <michel.lopes@student.griffith.ie>
2025-03-03 21:26:45 +0000

    Learnpython

---
+++ lecture-one.py
@@ -0,0 +1,43 @@
+"""#cost =10
+#tax_percet = .25
+#tax = cost * tax_percet
+#price = cost +tax
+#print (price)
+
+
+#username = "Codingwithteacher"
+#first_name = " teacher "
+#print (username + "" +first_name)
+
+#first_num = 10
+#second_num = 2
+#print (first_num)
+#print (second_num)
+
+#first_num = 1
+#print (first_num)
+# print (second_num)
+
+#going to print hello world
+print ("michel")
+"""
+#this is going over
+#multiple lines
+#lines
+
+"""""
+this is going over
+multiple lines
+lines
+"""
+wallet = 50
+
+item1 = 15
+tax = .03
+
+print ("you have $",wallet - item1 * tax)
+#print (wallet - item1 - item1 * tax)
+
+
+
+
