f9c38ce696a832605812419fa25c08d6b5c285a0
michel.lopes <michel.lopes@student.griffith.ie>
2025-03-10 09:50:10 +0000

    chatbot learn update --input=learn.json

---
+++ ForandWhileLoops.py
@@ -0,0 +1,11 @@
+"""
+for & while loops
+
+"""
+
+my_list = [1, 2, 3, 4, 5]
+print(my_list[0])
+print(my_list[1])
+print(my_list[2])
+print(my_list[3])
+print(my_list[4])
--- ifelse.py
+++ ifelse.py
@@ -1,11 +1,49 @@
 """flow control with if-elif-else"""
 
 
-x = 2
+"""x = 27
 
-if x > 1:
-    print("x is greater than 1")
+if x >= 3:
+    print("x is greater than ")
 else:
     print("x is less than 1")
 
-print("outside of if statement")
+print("outside of if statement")"""
+
+
+"""hour = 21
+
+if hour < 15:
+    print("Good morning")
+elif hour < 20: # elif = else if
+    print("Good afternoon")
+else:
+    print("Good evening")"""
+
+grade = 61-2
+
+if grade >= 90:
+            print("A")
+elif grade >= 80:
+            print("B")
+elif grade >= 70:
+            print("C")
+elif grade >= 60:
+            print("D")
+else:
+            print("F")
+
+#correction (both are correct)
+
+grade = 81
+
+if grade >= 90:
+            print("A")
+elif 80 <= grade < 90:
+            print("B")
+elif 70 <= grade < 80:
+            print("C")
+elif 60 <= grade < 70:
+            print("D")
+else:
+            print("F")
