b3e1894d3490d3d082842a1af6774c660eb69280
michel.lopes <michel.lopes@student.griffith.ie>
2025-03-12 18:40:39 +0000

    updating elearning

---
+++ Dictionary.py
@@ -0,0 +1,57 @@
+"""
+Dictionaries
+"""
+# Dictionaries are used to store data values in key:value pairs.
+# A dictionary is a collection which is unordered, changeable and does not allow duplicates.
+
+"""user_dictionary = {
+    'username': 'michel',
+    'name': 'Michel souza',
+    'age': 34,
+}"""
+#user_dictionary["married"] = True
+#print(user_dictionary.get('username'))# get the value of the key username
+#print(len(user_dictionary))# get the length of the dictionary
+
+#user_dictionary.pop('age')# remove the key age from the dictionary
+#print(user_dictionary)
+
+#user_dictionary.clear()# clear the dictionary
+#print(user_dictionary)
+
+#del user_dictionary# delete the dictionary
+
+#for x in user_dictionary: # loop through the dictionary
+   # print(x) # print the keys of the dictionary
+
+#for x, y in user_dictionary.items(): #
+    #print(x, y) # print the key:value pairs of the dictionary
+
+"""user_dictionary2 = user_dictionary.copy()
+user_dictionary2.pop('age')
+print(user_dictionary2)"""
+
+#exercise 2
+#create a for loop to print all keys and values
+#create a new variable vehicle2, which is a copy of my_vehicle
+#add a new key 'number_of_tires' to th vehicle 2 variable that is equal to 4
+#delete the mileage key and value from vehicle2
+#print just the keys from vehicle2
+
+
+my_vehicle = {# create a dictionary
+    'brand': 'ford',
+    'model': 'focus',
+    'year': 2019,
+    'mileage': 30000
+}
+
+vehicle = my_vehicle.copy()# copy the dictionary
+for x, y in my_vehicle.items():# loop through the dictionary
+    print(x, y)# print the key:value pairs of the dictionary
+
+vehicle['number_of_tires'] = 4# add a new key:value pair to the dictionary
+vehicle.pop('mileage')# remove the key:value pair from the dictionary
+print(vehicle.keys())
+
+
--- ForandWhileLoops.py
+++ ForandWhileLoops.py
@@ -3,9 +3,49 @@ for & while loops
 
 """
 
-my_list = [1, 2, 3, 4, 5]
+"""my_list = [1, 2, 3, 4, 5]
 print(my_list[0])
 print(my_list[1])
 print(my_list[2])
 print(my_list[3])
 print(my_list[4])
+
+sum_of_for_loop = 0
+for x in my_list:
+    sum_of_for_loop += x
+    print(sum_of_for_loop)"""
+
+
+"""my_list = ["monday, tuesday, wednesday, thursday, friday"]
+for x in my_list:
+    print(f"Today is {x}")"""
+
+"""i = 0
+while i < 5:
+    
+    i += 1
+    if i == 3:
+        continue
+    print(i)
+    if i == 4:
+        break
+else:
+    print("i is now larger or equal to 5")"""
+
+#exercise
+#1
+#Given: my_list = ["monday", "tuesday", "wednesday", "thursday", "friday"]
+#create a while loop that prints all elements of my_list variable 3 times
+#when printing the elements, use a for loop to print each character of the element
+#however, if the element of the for loop is equal to monday, continue withou prnting
+
+my_list = ["monday", "tuesday", "wednesday", "thursday", "friday"]
+i = 0
+while i < 3:# while i is less than 3 
+    for x in my_list:# for each element in my_list
+        i += 1 #increment i by 1 to avoid infinite loop
+        if x == "monday":#if x is equal to monday, continue to the next element 
+            print("--------")
+            continue
+        print(x)
+    
