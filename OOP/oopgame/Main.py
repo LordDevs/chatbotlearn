6d184677e4d1ee16596d904347e9776dd4d5377b
michel.lopes <michel.lopes@student.griffith.ie>
2025-03-19 14:16:34 +0000

    Refactor Main.py: Move code to oopgame/Main.py and clean up imports

---
+++ OOP/oopgame/Main.py
@@ -0,0 +1,33 @@
+"""from Animals import *
+
+dog = Dog()
+
+dog.legs
+dog.ears
+dog.type
+dog.age
+dog.name
+
+
+cat = Cat()
+
+cat.legs
+cat.ears
+cat.type
+cat.age
+cat.name"""
+
+"""
+from Enemy import *
+
+enemy = Enemy()
+
+enemy.type_of_enemy = 'the unknowns'
+
+print(f'{enemy.type_of_enemy} has {enemy.health_points} health points And can do attack of {enemy.attack_damage}')"""
+
+type_of_enemy: str
+health_points: int = 10
+attack_damage: int = 1
+
+print(health_points)
