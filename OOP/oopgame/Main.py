a51159a8cb4e64378b1b331d164056da954949ad
Michel <michel_wolf@hotmail.com>
2025-09-15 17:45:02 +0100

    New features. A little battle updating the loop

--- OOP/oopgame/Main.py
+++ OOP/oopgame/Main.py
@@ -1,6 +1,5 @@
-from Enemy import *
-from zombie import Zombie
-from Ogre import Ogre
+from Ogre import *
+from zombie import *
 
 def battle(e1: Enemy, e2: Enemy):
     e1.talk()
@@ -8,24 +7,25 @@ def battle(e1: Enemy, e2: Enemy):
 
     while e1.health_points > 0 and e2.health_points > 0:
         print('------------------')
+
         e1.special_attack()
         e2.special_attack()
         print(f'{e1.get_type_of_enemy()}: {e1.health_points} HP left')
         print(f'{e2.get_type_of_enemy()}: {e2.health_points} HP left')
 
         e2.attack()
-        e1.health_points -= e2.health_points
+        e1.health_points -= e2.attack_damage
         e1.attack()
-        e2.health_points -= e1.health_points
+        e2.health_points -= e1.attack_damage
 
-    print('------------------')
+        print('------------------')
 
     if e1.health_points > 0:
         print(f'{e1.get_type_of_enemy()} wins!')
     else:
         print(f'{e2.get_type_of_enemy()} wins!')
 
-zombie = Zombie(10, 2)   # HP=10, dano=2
+zombie = Zombie(10, 1)   # HP=10, dano=2
 ogre = Ogre(20, 3)       # HP=20, dano=3
 
 #print(f'{zombie.get_type_of_enemy()} has {zombie.health_points} health points and can attack for {zombie.attack_damage}')
